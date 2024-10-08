548bebcf1c69b12603183ab8bdbf49529851b757
dung <danghoangdung2004@gmail.com>
2024-10-08 23:02:38 +0200

    Fix trans in fbox

--- src/app/components/common/featured-boxes-2/featured-boxes-2.component.ts
+++ src/app/components/common/featured-boxes-2/featured-boxes-2.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -7,24 +7,19 @@ import { TranslateService } from '@ngx-translate/core';
     templateUrl: './featured-boxes-2.component.html',
     styleUrls: ['./featured-boxes-2.component.scss']
 })
-export class FeaturedBoxes2Component {
-
-    constructor(
-        public router: Router, private translate: TranslateService
-    ) { }
-
+export class FeaturedBoxes2Component implements OnInit {
     featuredBox = [
         {
-            icon: this.translate.instant('FBOX2.P1'),
-            title: this.translate.instant('FBOX2.C1')
+            icon: '',
+            title: ''
         },
         {
             icon: `20`,
-            title: this.translate.instant('FBOX2.C2')
+            title: ''
         },
         {
             icon: `15+`,
-            title: this.translate.instant('FBOX2.C3')
+            title: ''
         },
         {
             icon: `940`,
@@ -32,4 +27,26 @@ export class FeaturedBoxes2Component {
         }
     ]
 
+    constructor(
+        public router: Router, private translate: TranslateService
+    ) { }
+
+    ngOnInit(): void {
+        // Cập nhật bản dịch ban đầu
+        this.loadTranslations();
+  
+        // Đăng ký sự kiện thay đổi ngôn ngữ
+        this.translate.onLangChange.subscribe(() => {
+            this.loadTranslations();
+        });
+    }
+
+    loadTranslations() {
+        this.translate.get(['FBOX2.C1', 'FBOX2.C2', 'FBOX2.C3', 'FBOX2.P1']).subscribe(translations => {
+            this.featuredBox[0].icon = translations['FBOX2.P1'];
+            this.featuredBox[0].title = translations['FBOX2.C1'];
+            this.featuredBox[1].title = translations['FBOX2.C2'];
+            this.featuredBox[2].title = translations['FBOX2.C3'];
+        });
+    }
 }
--- src/app/components/common/featured-boxes-3/featured-boxes-3.component.ts
+++ src/app/components/common/featured-boxes-3/featured-boxes-3.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
@@ -7,12 +7,7 @@ import { TranslateService } from '@ngx-translate/core';
     templateUrl: './featured-boxes-3.component.html',
     styleUrls: ['./featured-boxes-3.component.scss']
 })
-export class FeaturedBoxes3Component {
-
-    constructor(
-        public router: Router, private translate: TranslateService
-    ) { }
-
+export class FeaturedBoxes3Component implements OnInit {
     featuredBox = [
         {
             icon: this.translate.instant('FBOX3.P1'),
@@ -32,4 +27,27 @@ export class FeaturedBoxes3Component {
         }
     ]
 
+    constructor(
+        public router: Router, private translate: TranslateService
+    ) { }
+
+    ngOnInit(): void {
+        // Cập nhật bản dịch ban đầu
+        this.loadTranslations();
+  
+        // Đăng ký sự kiện thay đổi ngôn ngữ
+        this.translate.onLangChange.subscribe(() => {
+            this.loadTranslations();
+        });
+    }
+
+    loadTranslations() {
+        this.translate.get(['FBOX3.P1', 'FBOX3.C1', 'FBOX3.C2', 'FBOX3.C3', 'FBOX3.C4']).subscribe(translations => {
+            this.featuredBox[0].icon = translations['FBOX3.P1'];
+            this.featuredBox[0].title = translations['FBOX3.C1'];
+            this.featuredBox[1].title = translations['FBOX3.C2'];
+            this.featuredBox[2].title = translations['FBOX3.C3'];
+            this.featuredBox[3].title = translations['FBOX3.C4'];
+        });
+    }
 }
